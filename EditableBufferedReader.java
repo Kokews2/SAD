2dac0c41fcf7e87e5770ed9e59613f07b19695c3
kokews2 <jorge.ortega@estudiantat.upc.edu>
2023-09-26 15:31:38 +0200

    Estructura básica de EditableBufferedReader.java

--- EditableBufferedReader.java
+++ EditableBufferedReader.java
@@ -1,3 +1,35 @@
-public class EditableBufferedReader {
-    
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Scanner;
+
+public class EditableBufferedReader extends BufferedReader {
+
+    public EditableBufferedReader(Reader in) {
+        super(in);
+    }
+
+    void setRaw(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("***** RAW MODE *****");
+
+        while(true){
+            if(scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }    
+        }
+    }
+
+    void unsetRaw() {
+
+    }
+
+    public int read() throws IOException {
+        return 0;
+    }
+
+    public String readLine() throws IOException {
+        return "";
+    }
 }
