7b86d3e5af1d311a8f4a78e223fa19d0d6de0b9f
kokews2 <jorge.ortega@estudiantat.upc.edu>
2023-09-26 16:59:02 +0200

    Creación del fichero writeints.c

---
+++ writeints.c
@@ -0,0 +1,13 @@
+/**
+ * Read integers from stdin and write them in binary form
+ * to stdout. Redirect output to intsc.bin:
+ * ...$ ./writeints.c >intsc.bin
+*/
+#include <stdio.h>
+
+int main() {
+    int i;
+    while (scanf("%d", &i) != EOF){
+        
+    }
+}
