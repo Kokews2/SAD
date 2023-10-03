843a1d0397675684b4f8fc695a7c82af7fcd1dc2
Kokews2 <jorge.ortega@estudiantat.upc.edu>
2023-10-03 12:17:20 +0200

    Creación métodos setRaw() y setCooked()

--- EditableBufferedReader.java
+++ EditableBufferedReader.java
@@ -1,28 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.Reader;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 public class EditableBufferedReader extends BufferedReader {
 
-    public EditableBufferedReader(Reader in) {
+    public EditableBufferedReader(InputStreamReader  in) {
         super(in);
     }
 
-    void setRaw(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("***** RAW MODE *****");
+    /*
+     * Cambia el mode del terminal a Raw 
+     * (Utilitzem la API de ProcessBuilder per executar comanes desde el terminal)
+     */
+    public void setRaw() throws InterruptedException {
+        try {
+            // Creem un procés que executi la comana 'stty -echo raw'
+            ProcessBuilder processBuilder = new ProcessBuilder("stty", "-echo", "raw");
+            Process process = processBuilder.start();
 
-        while(true){
-            if(scanner.hasNextLine()){
-                String line = scanner.nextLine();
-                System.out.println(line);
-            }    
+            // Esperamos a que el proceso termine
+            int exitCode = process.waitFor();
+
+            // Verifiquem que s'hagi executat bé la comana
+            if (exitCode == 0) {
+                System.out.println("Teclat en mode Raw");
+            } else {
+                System.out.println("Error al posar el teclat en mode Raw");
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
+    /*
+     * Cambia el mode del terminal a Cooked 
+     * (Utilitzem la API de ProcessBuilder per executar comanes desde el terminal)
+     */
     void unsetRaw() {
+        try {
+            // Creem un procés que executi la comana 'stty -echo raw'
+            ProcessBuilder processBuilder = new ProcessBuilder("stty", "-echo", "cooked");
+            Process process = processBuilder.start();
 
+            // Esperamos a que el proceso termine
+            int exitCode = process.waitFor();
+
+            // Verifiquem que s'hagi executat bé la comana
+            if (exitCode == 0) {
+                System.out.println("Teclat en mode Raw");
+            } else {
+                System.out.println("Error al posar el teclat en mode Raw");
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public int read() throws IOException {
