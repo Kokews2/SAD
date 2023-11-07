8c650472d37ae6cef0e0de9610da2baeb2683ae6
Pachito9 <pau.argiles@estudiantat.upc.edu>
2023-11-07 14:46:37 +0100

    ;exemple clase

--- EchoServer.java
+++ EchoServer.java
@@ -6,7 +6,17 @@ public class EchoServer{
 
         MyServerSocket ss = new MyServerSocket(Integer.parseInt(args[0]));
         while(true){
-            
+            //Esperar seguent conexio' client
+            MySocket s = ss.accept();
+            // Crear un thread auxiliar que atengiui ael client
+            new Thread(() -> {
+                String line;
+                while ((line = s.readLine()) != null){
+                    s.println(line);
+                }
+                s.close();
+            }).start();
+
         } 
     }
 }
