1928d6acd3645da0a48114b1bf282ef8fc31db04
Kokews2 <jorge.ortega@estudiantat.upc.edu>
2023-12-16 20:05:53 +0100

    Implementada segunda snake

--- Projecte/snakegame/src/main/java/snakegame/Ratoli.java
+++ Projecte/snakegame/src/main/java/snakegame/Ratoli.java
@@ -1,12 +1,7 @@
 package snakegame;
-import java.awt.MouseInfo;
-import java.awt.Point;
-import java.awt.PointerInfo;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 
 
 public class Ratoli implements MouseListener{
