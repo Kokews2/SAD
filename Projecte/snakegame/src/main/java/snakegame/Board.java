24cbff95709f9869341136df7a2f8a982367d35e
Pachito9 <pau.argiles@estudiantat.upc.edu>
2023-12-19 01:51:40 +0100

    JOC ACABAT

--- Projecte/snakegame/src/main/java/snakegame/Board.java
+++ Projecte/snakegame/src/main/java/snakegame/Board.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Board extends JPanel {
-    
+
     private Snake snake;
     private Snake snake2 = null;
     private Food food;
@@ -14,6 +14,7 @@ public class Board extends JPanel {
         this.snake = snake;
         this.food = food;
         this.walls = walls;
+        setBackground(new Color(34, 34, 34));
     }
 
     public void setSnake2(Snake snake2) {
@@ -28,33 +29,27 @@ public class Board extends JPanel {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        drawBoard(g);
+        
         drawSnake(g, snake, Color.GREEN, Color.YELLOW);
-        if(snake2 != null) drawSnake(g, snake2, Color.BLUE, Color.MAGENTA);
+        if (snake2 != null)
+            drawSnake(g, snake2, Color.CYAN, Color.MAGENTA);
         drawFood(g);
         drawScore(g);
-        if (!walls.getArrayDePuntos().isEmpty()) drawWalls(g);
-    }
-
-    private void drawBoard(Graphics g) {
-         // Dibujar el fondo del tablero (puedes personalizar esto)
-         g.setColor(Color.BLACK);
-         g.fillRect(0, 0, getWidth(), getHeight());
- 
-         // Dibujar el rectángulo exterior de negro
-         g.setColor(Color.BLACK);
-         g.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
+        if (!walls.getArrayDePuntos().isEmpty())
+            drawWalls(g);
     }
 
     private void drawSnake(Graphics g, Snake snake, Color color1, Color color2) {
         int i = 0;
         for (Point point : snake.getBody()) {
-            if(i%2 == 0) g.setColor(color1);
-            if(i%2 == 1) g.setColor(color2);
+            if (i % 2 == 0)
+                g.setColor(color1);
+            if (i % 2 == 1)
+                g.setColor(color2);
             g.fillOval(point.x, point.y, snake.getWidth(), snake.getHeight());
             i++;
         }
-    }    
+    }
 
     private void drawFood(Graphics g) {
         Point point = food.getFood();
@@ -63,9 +58,18 @@ public class Board extends JPanel {
     }
 
     public void drawScore(Graphics g) {
-        g.setColor(Color.BLACK);
+        Font font = new Font("Arial", Font.ITALIC, 14);
+        g.setFont(font);
+
+        g.setColor(Color.GREEN);
         g.drawString("Player 1 (Green): " + snake.getScore(), 10, 20);
-        if(snake2 != null) g.drawString("Player 2 (Blue): " + snake2.getScore(), 10, 40);
+
+        if (snake2 != null) {
+            g.setColor(Color.CYAN);
+            Font fontPlayer2 = new Font("Arial", Font.ITALIC, 14);
+            g.setFont(fontPlayer2);
+            g.drawString("Player 2 (Blue): " + snake2.getScore(), 10, 40);
+        }
     }
 
     public void drawWalls(Graphics g) {
